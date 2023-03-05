8a45e0e0414e08bd18eba020ca91ee63d252dd56
Josh Grainge <josh.grainge@hotmail.com>
2023-03-05 12:19:10 -0500

    Displaying new meme on button press (from mock database file)

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import memeData from '../memeData';
 
 export default function Meme(props) {
+  const [url, setUrl] = useState();
+
+  const getRandomMemeUrl = () => {
+    const memes = memeData.data.memes;
+    const randomIndex = Math.floor(Math.random() * memes.length);
+    setUrl(memes[randomIndex].url);
+  };
+
   return (
-    <form className="meme-form">
+    <div className="meme-form">
       <div className="inputs">
         <input placeholder="Shut up" />
         <input placeholder="and take my money" />
       </div>
-      <button type="submit" className="new-meme-btn">
+      <button className="new-meme-btn" onClick={getRandomMemeUrl}>
         Get a new meme image 🖼️
       </button>
-    </form>
+
+      <img src={url} alt="meme" className="meme-image" />
+    </div>
   );
 }
