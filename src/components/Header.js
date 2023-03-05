780ce7f45b39d8a312ff364543bcf5b71af65ec9
Josh Grainge <josh.grainge@hotmail.com>
2023-03-04 20:55:10 -0500

    Added mouse over event to troll face

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,14 @@ export default function Header(props) {
   return (
     <div className="header">
       <div className="logo-container">
-        <img src={trollFace} alt="le-troll-face" className="logo" />
+        <img
+          src={trollFace}
+          alt="le-troll-face"
+          className="logo"
+          onMouseOver={() => {
+            console.log('Hovered over troll face');
+          }}
+        />
         <h1>Meme Generator</h1>
       </div>
       <p>React Course - Project 3</p>
