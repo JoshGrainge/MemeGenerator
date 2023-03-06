81c112351727675a024539c51dd699ee41acd962
Josh Grainge <josh.grainge@hotmail.com>
2023-03-05 23:14:36 -0500

    Added state to meme component

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -2,25 +2,57 @@ import React, { useState } from 'react';
 import memeData from '../memeData';
 
 export default function Meme(props) {
-  const [url, setUrl] = useState();
+  const [allMemeImages, setAllMemeImages] = useState(memeData.data.memes);
+
+  const [meme, setMeme] = useState({
+    topText: 'One does not simply',
+    bottomText: 'Walk into Mordor',
+    randomImage: 'http://i.imgflip.com/1bij.jpg',
+  });
 
   const getRandomMemeUrl = () => {
-    const memes = memeData.data.memes;
-    const randomIndex = Math.floor(Math.random() * memes.length);
-    setUrl(memes[randomIndex].url);
+    const randomIndex = Math.floor(Math.random() * allMemeImages.length);
+    const url = allMemeImages[randomIndex].url;
+    setMeme((prev) => {
+      return { ...prev, randomImage: url };
+    });
   };
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setMeme((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  }
 
   return (
     <div className="meme-form">
       <div className="inputs">
-        <input placeholder="Shut up" />
-        <input placeholder="and take my money" />
+        <input
+          placeholder="Shut up"
+          name="topText"
+          value={meme.topText}
+          onChange={handleChange}
+        />
+        <input
+          placeholder="and take my money"
+          name="bottomText"
+          value={meme.bottomText}
+          onChange={handleChange}
+        />
       </div>
       <button className="new-meme-btn" onClick={getRandomMemeUrl}>
         Get a new meme image 🖼️
       </button>
 
-      <img src={url} alt="meme" className="meme-image" />
+      <div className="meme">
+        <img src={meme.randomImage} alt="meme" className="meme-image" />
+        <h2 className="top-text">{meme.topText}</h2>
+        <h2 className="bottom-text">{meme.bottomText}</h2>
+      </div>
     </div>
   );
 }
